c7427123f44c00735e564965a065eb134c74ac1b
Iuri Almeida <iurilopesalmeida@gmail.com>
2021-05-14 12:39:35 -0300

    Herança múltipla - com ComboDevice

--- heranca_multipla/src/application/Program.java
+++ heranca_multipla/src/application/Program.java
@@ -1,21 +1,29 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Printer p = new Printer("1080");
+		ConcretePrinter p = new ConcretePrinter("1080");
 		p.processDoc("My letter");
 		p.print("My letter");
 		
 		System.out.println();
 		
-		Scanner s = new Scanner("2023");
+		ConcreteScanner s = new ConcreteScanner("2023");
 		s.processDoc("My e-mail");
 		System.out.println("Scan  result: " + s.scan());
+		
+		System.out.println();
+		
+		ComboDevice c = new ComboDevice("5224");
+		c.processDoc("My TCC");
+		c.print("My TCC");
+		System.out.println("Scan result: " + c.scan());
 
 	}
 
---
+++ heranca_multipla/src/devices/ComboDevice.java
@@ -0,0 +1,25 @@
+package devices;
+
+public class ComboDevice extends Device implements Scanner, Printer {
+
+	public ComboDevice(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void print(String doc) {
+		System.out.println("Combo Printing: " + doc);
+		
+	}
+
+	@Override
+	public String scan() {
+		return "Combo Scanned content";
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Combo Printer processing: " + doc);
+	}
+
+}
