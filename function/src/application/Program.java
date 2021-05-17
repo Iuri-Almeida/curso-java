8e38a73cf02a45a90a817e6acaded85e1ab7af84
Iuri Almeida <iurilopesalmeida@gmail.com>
2021-05-17 12:52:13 -0300

    Function - Versão implementação da interface

---
+++ function/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import model.entities.Product;
+import model.util.UppercaseName;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<Product>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		List<String> names = list.stream().map(new UppercaseName()).collect(Collectors.toList());
+		
+		for (String name : names) System.out.println(name);
+		
+	}
+
+}
