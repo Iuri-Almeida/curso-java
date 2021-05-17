c22dc086ddaf403a68586e8365988183b27a7880
Iuri Almeida <iurilopesalmeida@gmail.com>
2021-05-17 12:33:44 -0300

    Consumer - Versão reference method com método NÃO estático

--- consumer/src/model/entities/Product.java
+++ consumer/src/model/entities/Product.java
@@ -33,8 +33,8 @@ public class Product {
 		p.setPrice(p.getPrice() * 1.1);
 	}
 	
-	public boolean nonStaticProductPredicate() {
-		return price >= 100;
+	public void nonStaticProductPriceUpdate() {
+		price *= 1.1;
 	}
 	
 	@Override
