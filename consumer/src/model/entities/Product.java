aae658ba8647522d90d5f358c21f755fc4407fbb
Iuri Almeida <iurilopesalmeida@gmail.com>
2021-05-17 12:32:29 -0300

    Consumer - Versão reference method com método estático

--- consumer/src/model/entities/Product.java
+++ consumer/src/model/entities/Product.java
@@ -29,8 +29,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public static boolean staticProductPredicate(Product p) {
-		return p.getPrice() >= 100;
+	public static void staticProductPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
 	}
 	
 	public boolean nonStaticProductPredicate() {
