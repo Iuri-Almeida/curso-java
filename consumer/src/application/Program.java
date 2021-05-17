e1c3d1d2c29854293261640d241f492c702c8eea
Iuri Almeida <iurilopesalmeida@gmail.com>
2021-05-17 12:36:50 -0300

    Consumer - Versão expressão lambda declarada

--- consumer/src/application/Program.java
+++ consumer/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import model.entities.Product;
 
@@ -16,7 +17,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticProductPriceUpdate);
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);;
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 		
