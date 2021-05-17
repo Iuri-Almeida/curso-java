2fe2e1a13d397a3878585fad51f992585cebba37
Iuri Almeida <iurilopesalmeida@gmail.com>
2021-05-17 12:59:05 -0300

    Function - Versão expressão lambda declarada

--- function/src/application/Program.java
+++ function/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import model.entities.Product;
@@ -17,7 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> names = list.stream().map(Product::nonStaticProductUppercaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		for (String name : names) System.out.println(name);
 		
